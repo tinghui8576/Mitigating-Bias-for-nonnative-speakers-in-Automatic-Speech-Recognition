95f11c38e6465fd8e899c3c870464cc5a8dd5684
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2025-04-09 18:04:02 +0200

    Create Dataset format fits for non Hugging face data

---
+++ CustomData/create_data_path.py
@@ -0,0 +1,28 @@
+import os
+path = '/Users/tinghui/Downloads/l2arctic_release_v5.0'
+audio_dirs = os.listdir(path)
+audio_dirs
+audio_extensions = {".mp3", ".wav", ".m4a", ".aac", ".flac", ".ogg", ".wma"}
+audio_entries = []
+text_entries = []
+for dirpath, _, filenames in os.walk(path):
+    for filename in filenames:
+        if filename.endswith(".wav"):
+            audio_path = os.path.join(dirpath, filename)
+            rel_path = os.path.relpath(dirpath, path)
+            utt_id = "_".join([rel_path.split(os.sep)[0]] + [os.path.splitext(filename)[0]])
+            
+
+            transcript_path = os.path.join(path, rel_path.split(os.sep)[0], "transcript", os.path.splitext(filename)[0]+'.txt')
+            if os.path.exists(transcript_path):
+                with open(transcript_path, "r", encoding="utf-8") as f:
+                    transcript = f.read().strip()
+                    text_entries.append(f"{utt_id} {transcript}")
+                    audio_entries.append(f"{utt_id} {os.path.abspath(audio_path)}")
+
+
+with open("L2/audio_paths", "w", encoding="utf-8") as f:
+    f.write("\n".join(audio_entries))
+
+with open("L2/text", "w", encoding="utf-8") as f:
+    f.write("\n".join(text_entries))              
