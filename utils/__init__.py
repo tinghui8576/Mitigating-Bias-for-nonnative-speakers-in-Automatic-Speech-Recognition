1b5ff036d8f3744d19229f25560a79c8e8dcdb02
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2025-08-14 22:16:11 +0200

    Clean Unused transcript

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .WhisperDataset import WhisperDataset
-from .useage import remove_duplicates, extract_age, collate_fn
-# from .Model import Whisper
-# from utils.
-# from utils.Adapter import AccentAdapter
