a6168b65b3b6db5079a5d689958a9249d1b415f6
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2025-08-17 21:01:26 +0200

    Update code for cleaner version

--- transcribe_eval.py
+++ transcribe_eval.py
@@ -10,30 +10,7 @@ from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from utils.useage import chunk_audio, transcribe_chunks, clean_transcript
 from utils.eval import metrics
 from utils.constant import sentence
-
-def add_leace(model):
-    eraser_dir = "models/leace_erasers/"
-    erasers = []
-    layers_to_hook = [11]
-    
-    # Load erasers for these layers
-    for i in layers_to_hook:
-        path = os.path.join(eraser_dir, f"leace_eraser_layer{i}.pt")
-        erasers.append(torch.load(path))
-
-    # Register hooks only on these layers
-    for layer_idx, eraser in zip(layers_to_hook, erasers):
-        layer = model.model.encoder.layers[layer_idx]
-        
-        def make_hook(my_eraser):
-            def hook_fn(module, input, output):
-                hidden_states = output[0]
-                erased = my_eraser(hidden_states.squeeze(0)).unsqueeze(0)
-                return (erased,)
-            return hook_fn
-        
-        layer.register_forward_hook(make_hook(eraser))
-    return model
+from Model.utils import add_leace
 
 def main(args):
     args.device = torch.device(f"cuda:{args.device}" if args.device >= 0 else "cpu")
@@ -58,7 +35,7 @@ def main(args):
         model = WhisperForConditionalGeneration.from_pretrained(model_id)
         processor = WhisperProcessor.from_pretrained(model_id, language=args.language, task="transcribe")
         if args.strategy == "leace":
-            model = add_leace(model)
+            model = add_leace(model, args.ckpt_dir)
 
     model.to(args.device)
 
